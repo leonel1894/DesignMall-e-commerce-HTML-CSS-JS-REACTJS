8266c3101d71d4e18e8d68bd560bd19b1903a7c3
leonel1894 <leonelalfonsin1894@gmail.com>
2023-03-09 19:14:45 -0300

    arreglo cart

--- src/components/CartWidget/CartWidget.js
+++ src/components/CartWidget/CartWidget.js
@@ -1,7 +1,7 @@
 const CartWidget = () => {
     return (
         <button className="btn btn-outline-dark d-flex gap-2 ms-4">
-            <i class="bi bi-cart"></i>0
+            <i class="bi bi-cart"></i> {0}
         </button>
     )
 }
